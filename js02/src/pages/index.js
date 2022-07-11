3dfaca7c3f3df3b2b552d44c9bf1cf1b0efa9ed1
dexn <sunda0707@icloud.com>
2022-07-11 23:22:07 +0900

    wip

--- js02/src/pages/index.js
+++ js02/src/pages/index.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./styles.scss";
 
 const IndexPage = () => {
-  return <div>hoge</div>;
+  return (
+    <div>
+      <input id="acd-check1" className="acd-check" type="checkbox" />
+      <label className="acd-label" htmlFor="acd-check1">
+        クリックで開く1
+      </label>
+      <div className="acd-content">
+        <p>hello.world!</p>
+      </div>
+    </div>
+  );
 };
 
 export default IndexPage;
