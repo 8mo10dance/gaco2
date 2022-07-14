9c93178a753ebc347449a2adfd0082819e041ed8
dexn <sunda0707@icloud.com>
2022-07-14 18:52:34 +0900

    wip2

--- js03/main.js
+++ js03/main.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('hoge')
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const addButton = document.getElementById("add-button");
+  addButton.addEventListener("click", () => {
+    console.log("hoge");
+  });
+  const editButtons = document.querySelectorAll("#list .js-edit-button");
+  for (let b of editButtons) {
+    b.addEventListener("click", (e) => {
+      console.log(e.target.closest("li").dataset.id);
+    });
+  }
+});
