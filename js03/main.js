2e5fb4f976a356cfd8bf33365911a0878d27acd9
dexn <sunda0707@icloud.com>
2022-07-14 18:57:39 +0900

    wip3

--- js03/main.js
+++ js03/main.js
@@ -1,12 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   const addButton = document.getElementById("add-button");
   addButton.addEventListener("click", () => {
-    console.log("hoge");
+    dispatch(Action("ADD_ITEM"));
   });
-  const editButtons = document.querySelectorAll("#list .js-edit-button");
-  for (let b of editButtons) {
-    b.addEventListener("click", (e) => {
-      console.log(e.target.closest("li").dataset.id);
+  for (let btn of document.querySelectorAll("#list .js-edit-button")) {
+    btn.addEventListener("click", (elem) => {
+      const li = elem.target.closest("li");
+      dispatch(
+        Action("EDIT_ITEM", {
+          id: li.dataset.id,
+          value: li.querySelector("input").value,
+        })
+      );
+    });
+  }
+  for (let btn of document.querySelectorAll("#list .js-delete-button")) {
+    btn.addEventListener("click", (elem) => {
+      dispatch(
+        Action("DELETE_ITEM", { id: elem.target.closest("li").dataset.id })
+      );
     });
   }
 });
+
+const Action = (type, options = {}) => ({
+  type,
+  ...options,
+});
+
+const dispatch = (action) => {
+  console.log(action);
+};
