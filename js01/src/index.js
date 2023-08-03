8b5a58672baef8f5dc9d7be4f6379c1a51b5eb25
tomoya sunda <dexn.dev@outlook.jp>
2023-08-03 19:48:34 +0900

    サンプルコードを作成する

--- js01/src/index.js
+++ js01/src/index.js
@@ -1,5 +1,7 @@
-import './theme.css'
+import 'slick-carousel'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 $(function () {
-  console.log("hoge")
+  $('.single-item').slick();
 })
