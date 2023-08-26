1ad1d00d1eaa3c4ca74da4f9a7ab1e2c5b24c6ca
tomoya sunda <dexn.dev@outlook.jp>
2023-08-26 18:18:55 +0900

    react-loading-overlay のサンプルを作成する

--- js02/src/index.js
+++ js02/src/index.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useCallback } from 'react';
 import { createRoot } from "react-dom/client";
+import LoadingOverlay from 'react-loading-overlay-ts';
 
 const App = () => {
-  return <div>hoge</div>;
-};
+  const [isActive, setActive] = useState(true)
+  setTimeout(() => setActive(value => !value), 3000)
+
+  return (
+    <LoadingOverlay
+      active={isActive}
+      spinner
+      text='Loading your content...'
+    >
+      <div style={{ height: '100vh' }}>
+        <p>Some content or children or something.</p>
+      </div>
+    </LoadingOverlay>
+  )
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("root");
