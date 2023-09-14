f0ee13bbd8486bc36cdf5a4572f452e3aa055c63
sunda <sunda@synchro-food.co.jp>
2023-09-14 17:27:16 +0900

    (#14) carrierwave を localstack S3 につなぐ

---
+++ r01/config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+CarrierWave.configure do |config|
+  config.aws_attributes = {
+    cache_control: 'max-age=2592000, s-maxage=2592000',
+  }
+  config.storage = :aws
+  config.cache_storage = :aws
+  config.asset_host = "#{ENV['S3_ASSET_HOST']}/#{ENV['S3_BUCKET']}"
+  config.aws_credentials = {
+    region:            ENV.fetch('S3_REGION'),
+    access_key_id:     ENV.fetch('S3_ACCESS_KEY'),
+    secret_access_key: ENV.fetch('S3_SECRET_KEY'),
+  }
+end
