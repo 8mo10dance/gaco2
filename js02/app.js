06da391d7c886fb7b7fc5f67f2ad06e26ef8e608
tomoya sunda <dexn.dev@outlook.jp>
2023-10-19 19:11:03 +0900

    (#21) ページ遷移できるようにする

--- js02/app.js
+++ js02/app.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 9000
 
 app.use('/', express.static('public/home'))
+app.use('/about', express.static('public/about'))
+app.use('/contact', express.static('public/contact'))
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
