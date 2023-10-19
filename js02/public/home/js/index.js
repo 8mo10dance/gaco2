e9ebb125b05d2d2c803723bf204214f862927b92
tomoya sunda <dexn.dev@outlook.jp>
2023-10-19 19:25:14 +0900

    画面遷移の例

--- js02/public/home/js/index.js
+++ js02/public/home/js/index.js
@@ -1,1 +1,7 @@
-console.log('hoge')
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('js-btn')
+  btn.onclick = () => {
+    const box = document.getElementById('js-box')
+    box.innerText = 'clicked'
+  }
+})
