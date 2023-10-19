008af472a1595c02d3ea67f4d8463b1180187c8e
tomoya sunda <dexn.dev@outlook.jp>
2023-10-19 19:01:18 +0900

    (#21) 静的なページを表示する

--- js02/app.js
+++ js02/app.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const port = 9000
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.use('/', express.static('public/home'))
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`)
