62acaa860842861a0a94f2edda146a0b1c5648e7
tomoya sunda <dexn.dev@outlook.jp>
2023-10-24 15:27:58 +0900

    (#14) Migrate

--- r03/db/schema.rb
+++ r03/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_23_060003) do
+ActiveRecord::Schema.define(version: 2023_10_24_062720) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
 end
