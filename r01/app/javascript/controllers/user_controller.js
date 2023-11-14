1b5b376955c829b7820aed05a44e65ea6fdfecfb
sunda <sunda@synchro-food.co.jp>
2023-11-14 10:05:46 +0900

    (#34) 署名付きURLを使って画像をアップロードする

--- r01/app/javascript/controllers/user_controller.js
+++ r01/app/javascript/controllers/user_controller.js
@@ -3,10 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 import { apiRequest } from '../helpers/api'
 
 export default class extends Controller {
-  static targets = ['avatarIdField']
+  static targets = ['avatarIdField', 'avatarImageArea']
 
   set avatarId(avatarId) {
     this.avatarIdFieldTarget.value = avatarId
+  }
+
+  set avatarImageUrl(imageUrl) {
+    const imageEl = document.createElement('img')
+    imageEl.src = imageUrl
+    this.avatarImageAreaTarget.appendChild(imageEl)
   }
 
   async changeAvatar(event) {
@@ -14,6 +20,12 @@ export default class extends Controller {
     if (!file) return
 
     try {
+      const url = await getUploadUrl(file)
+      const imageUrl = await uploadAvatar(url, file)
+      if (imageUrl) {
+        this.avatarImageUrl = imageUrl
+      }
+
       const response = await uploadUserAvatar(file)
       if (response.ok) {
         const data = await response.json()
@@ -30,4 +42,45 @@ function uploadUserAvatar(file) {
   formData.append("user_avatar[image]", file)
 
   return apiRequest('/api/v1/user_avatars', { method: 'POST', body: formData })
+}
+
+async function getUploadUrl(file) {
+  try {
+    const response = await fetch(`/api/v1/avatar_upload_url?filename=${file.name}&content_type=${file.type}`)
+    if (response.ok) {
+      const data = await response.json()
+      return data.url
+    }
+
+    return null
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function uploadAvatar(url, file) {
+  const formData = new FormData()
+  formData.append('file', file)
+
+  const imageUrl = file => {
+    return `http://localhost:4566/microposts/tmp_uploads/${file.name}`
+  }
+
+  try {
+    const response = await fetch(
+      url,
+      {
+        method: 'PUT',
+        accept: 'multipart/form-data',
+        body: formData
+      }
+    )
+    if (response.ok) {
+      return imageUrl(file)
+    }
+
+    return null
+  } catch (err) {
+    console.log(err)
+  }
 }
