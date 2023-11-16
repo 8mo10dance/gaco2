c23c15d7926511d517d337605d90a1d572c90131
tomoya sunda <dexn.dev@outlook.jp>
2023-11-16 20:04:48 +0900

    (#51) サーバー側で EXIF を取得する

--- r01/app/uploaders/avatar_uploader.rb
+++ r01/app/uploaders/avatar_uploader.rb
@@ -1,7 +1,14 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+
+  process :get_exif_info
+
+  def get_exif_info
+    exif = Magick::Image.read(self.file.file).first.properties
+    binding.pry
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
