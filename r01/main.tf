a3c54f5d3e20b47c45ecdf2b894e97f958cc887e
tomoya sunda <dexn.dev@outlook.jp>
2023-11-27 09:54:30 +0900

    (#59) uploads/ 下のファイルだけEXIF除去する

--- r01/main.tf
+++ r01/main.tf
@@ -52,6 +52,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.my_lambda.arn
     events              = ["s3:ObjectCreated:*"]
+    filter_prefix = "uploads/"
   }
 }
 
