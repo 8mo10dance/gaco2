2265f84cd884e3f1322847a339350bf8c3b0bd0c
tomoya sunda <dexn.dev@outlook.jp>
2024-01-23 20:01:00 +0900

    Todo コンポーネントを実装する

--- js01/src/TodoList/Todo/index.js
+++ js01/src/TodoList/Todo/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-export default function Todo() {
+export default function Todo({
+  value = "",
+  onChange = (e) => console.log(e.target.value),
+  onRemove = () => console.log("remove"),
+}) {
   return (
     <div>
-      <input type="text" />
-      <button>Remove</button>
+      <input type="text" value={value} onChange={onChange} />
+      <button onClick={onRemove}>Remove</button>
     </div>
   );
 }
