ebd449bfa359b6750c07b798807bc4fcb879b64e
tomoya sunda <dexn.dev@outlook.jp>
2024-01-25 16:51:57 +0900

    people テーブルのシードを作成する
    $ php artisan make:seeder PeopleTableSeeder

---
+++ php03/server/database/seeds/PeopleTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PeopleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+            'name' => 'sunda',
+            'mail' => 'sunda@example.com',
+            'age' => 12,
+        ];
+        DB::table('people')->insert($param);
+    }
+}
