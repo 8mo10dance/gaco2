5063b7189d92a22efe91944339f782c68eaa1e85
tomoya sunda <dexn.dev@outlook.jp>
2024-02-07 16:26:15 +0900

    wip

--- js01/src/index.js
+++ js01/src/index.js
@@ -2,8 +2,22 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./theme.css";
 import App from "./App";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: "dummy",
+};
 
 document.addEventListener("DOMContentLoaded", () => {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Firebase Authentication and get a reference to the service
+  const auth = getAuth(app);
+
   const container = document.getElementById("root");
   const root = createRoot(container);
   root.render(<App />);
